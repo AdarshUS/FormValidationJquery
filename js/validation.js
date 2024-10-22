b98055173ffe1fefbc8d41ef71ba07f5434921f9
adarsh <adarsh.us@techversantinfotech.com>
2024-10-22 10:34:32 +0530

    completed form validation using jquery

--- js/validation.js
+++ js/validation.js
@@ -1,32 +1,56 @@
 $(function() { 
-  var $form = $("#myform");
-  if ($form.length) {
-      $form.validate({
-         rules: {
-           Name: {
-              required: true
-                  },
-            phone:{
-                required: true,
-                 pattern:"[0-9]+",minlength:10, maxlength:10
-                 },
-             newpassword:{
-                required: true,
-                 }
-                },
-            messages: {  
-              Name: {
-required: 'Username is mandatory!'
-                    },
-             phone:{
-                required:'Enter a valid Phone Number'
-                 },
-               newpassword:{
-                   required:'Password cannot be empty'
-                  }
-                       }
-                 });
-                   }
-                    });
+var $form = $("#myform");
+if ($form.length) {
 
- 
+var value = $("#newpassword").val();
+
+$.validator.addMethod("checklower", function(value) {
+  return /[a-z]/.test(value);
+});
+$.validator.addMethod("checkupper", function(value) {
+  return /[A-Z]/.test(value);
+});
+$.validator.addMethod("checkdigit", function(value) {
+  return /[0-9]/.test(value);
+});
+
+
+$form.validate({
+rules: {
+Name: {
+required: true
+},
+phone: {
+required: true,
+minlength: 10,
+maxlength: 10
+},
+newpassword: {
+required: true,
+minlength: 6,
+maxlength: 30,
+checklower: true,
+checkupper: true,
+checkdigit: true
+},
+confirmpassword: {
+equalTo: "#newpassword"
+}
+},
+messages: {  
+Name: {
+required: 'Username is mandatory!'
+},
+phone: {
+required: 'Enter a valid Phone Number'
+},
+newpassword: {
+required: 'Password cannot be empty',
+checklower: 'Need at least 1 lowercase alphabet',
+checkupper: 'Need at least 1 uppercase alphabet',
+checkdigit: 'Need at least 1 digit'
+}
+}
+});
+}
+});
